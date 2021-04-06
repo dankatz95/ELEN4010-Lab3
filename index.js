3bc42f7835866c4715bf873c959760232edb53e7
Daniel Katz <1854147@students.wits.ac.za>
2021-04-06 22:26:33 +0200

    Added photo

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const button = document.getElementById('addButton')
 
 button.addEventListener('click', function () {
   const paragraph = document.createElement('p')
-  const text = document.createTextNode('Daniel Katz')
-  const yomama = document.createTextNode('Yo Mama')
+  const text = document.createTextNode('This is a student')
   paragraph.appendChild(text)
-  paragraph.appendChild(yomama)
-  document.body.appendChild(paragraph)
+  const students = document.getElementById('studentList')
+  students.appendChild(paragraph)
+}, false)
+
+const photoButton = document.getElementById('addPhoto')
+
+photoButton.addEventListener('click', () => {
+  const image = document.createElement('img')
+  image.src = 'A1_Visa.jpg'
+  image.style.width = '100px'
+  document.body.appendChild(image)
 }, false)
