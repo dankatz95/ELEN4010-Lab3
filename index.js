fd76218d90c966d5c5f2d45df9c9b6ad59421152
Daniel Katz <1854147@students.wits.ac.za>
2021-04-07 11:30:01 +0200

    Exercise 2.1 hide/show feature

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
-const button = document.getElementById('addButton')
+'use strict'
+const students = [
+  {
+    name: 'Daniel',
+    studentNumber: 1854147
+  },
+  {
+    name: 'Daniella',
+    studentNumber: 1479236
+  },
+  {
+    name: 'Sean',
+    studentNumber: 1876254
+  },
+  {
+    name: 'Themba',
+    studentNumber: 851478
+  },
+  {
+    name: 'Johan',
+    studentNumber: 1456987
+  }
+]
 
-button.addEventListener('click', function () {
-  const paragraph = document.createElement('p')
-  const text = document.createTextNode('This is a student')
-  paragraph.appendChild(text)
-  const students = document.getElementById('studentList')
-  students.appendChild(paragraph)
-}, false)
+const studentHolder = document.getElementById('myDiv')
 
-const photoButton = document.getElementById('addPhoto')
+for (let i = 0; i < 5; i++) {
+  const student = document.createElement('p')
 
-photoButton.addEventListener('click', () => {
-  const image = document.createElement('img')
-  image.src = 'A1_Visa.jpg'
-  image.style.width = '100px'
-  document.body.appendChild(image)
-}, false)
+  const text = document.createTextNode(`Name: ${students[i].name} --- Std num:  ${students[i].studentNumber}`)
+
+  student.appendChild(text)
+
+  studentHolder.appendChild(student)
+}
+
+document.getElementById('myDiv').style.display = 'none'
+
+let a = 1
+
+const showHide = function () {
+  if (a === 1) {
+    document.getElementById('myDiv').style.display = 'block'
+    return (a = 0)
+  } else {
+    document.getElementById('myDiv').style.display = 'none'
+    return (a = 1)
+  }
+}
+
+const view = document.getElementById('viewButton')
+
+view.addEventListener('click', showHide, false)
